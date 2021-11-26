af753cb5e6f151833b7673c869b04e6445bbc9fd
Sven Mol <s.mol@student.utwente.nl>
2021-11-26 22:20:48 +0100

    2019-05-1

---
+++ 2019/5/part1.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import {readFileSync} from "fs";
+import IntCode from "../intcode";
+
+(async () => {
+    const cookie = readFileSync("cookie.txt").toString();
+    const machine = new IntCode([1, 2, 3, 4, 99]);
+    const data = await axios.get("https://adventofcode.com/2019/day/5/input", {headers: {cookie}});
+    const program = data.data.split(",").map(v => parseInt(v));
+    machine.execute(program, [1]);
+    const output = machine.getBufferOut();
+    console.log("The answer is", output[output.length - 1]);
+})();
--- 2019/intcode.ts
+++ 2019/intcode.ts
@@ -1,38 +1,60 @@
 const ops: { [key: string]: (m: IntCode) => void } = {
     1: machine => machine.write(
-        machine.offsetRead(3),
-        machine.read(machine.offsetRead(1)) + machine.read(machine.offsetRead(2)),
+        machine.readOffset(3),
+        machine.readArgument(1) + machine.readArgument(2),
     ),
     2: machine => machine.write(
-        machine.offsetRead(3),
-        machine.read(machine.offsetRead(1)) * machine.read(machine.offsetRead(2)),
+        machine.readOffset(3),
+        machine.readArgument(1) * machine.readArgument(2),
+    ),
+    3: machine => machine.write(
+        machine.readOffset(1),
+        machine.bufferReadIn(),
+    ),
+    4: machine => machine.bufferWriteOut(
+        machine.readArgument(1)
     ),
     99: machine => machine.terminate(),
 };
 
+const opWidth: { [key: keyof typeof ops]: number } = {
+    1: 4,
+    2: 4,
+    3: 2,
+    4: 2,
+    99: 1,
+};
+
 export default class IntCode {
     private enabledOps: string[];
+
     private data: number[] = [];
+    private inBuffer: number[] = [];
+    private outBuffer: number[] = [];
+
     private pointer = 0;
     private running = false;
 
     constructor(enabled: (string | number)[] = Object.keys(ops)) {
-        this.enabledOps = enabled.map(v => v.toString());
+        this.enabledOps = enabled.map(v => v.toString()).filter(code => code in ops);
     }
 
-    execute(data: number[]): number[] {
+    execute(data: number[], inBuffer: number[] = []): number[] {
         this.data = data;
+        this.inBuffer = inBuffer;
+        this.outBuffer = [];
+
         this.pointer = 0;
         this.running = true;
 
         while (this.running) {
-            const opCode = this.data[this.pointer].toString();
+            const opCode = (this.data[this.pointer] % 100).toString();
             if (this.enabledOps.indexOf(opCode) === -1) {
                 throw new Error(`Encountered unknown or disabled opcode [${opCode}]`);
             }
 
             ops[opCode](this);
-            this.pointer += 4;
+            this.pointer += opWidth[opCode];
         }
 
         return this.data;
@@ -46,12 +68,34 @@ export default class IntCode {
         this.data[index] = data;
     }
 
-    offsetRead(offset: number): number {
+    readOffset(offset: number): number {
         return this.read(this.pointer + offset);
     }
 
-    offsetWrite(offset: number, data: number): void {
+    writeOffset(offset: number, data: number): void {
         this.write(this.pointer + offset, data);
+    }
+
+    readArgument(argument: number): number {
+        const argValue = this.readOffset(argument);
+        return this.isImmediate(argument) ? argValue : this.read(argValue);
+    }
+
+    isImmediate(argument: number): boolean {
+        const opFlags = Math.floor(this.data[this.pointer] / 100).toString();
+        return opFlags.charAt(opFlags.length - argument) === "1";
+    }
+
+    bufferReadIn(): number {
+        return this.inBuffer.shift();
+    }
+
+    bufferWriteOut(value: number): void {
+        this.outBuffer.push(value);
+    }
+
+    getBufferOut(): number[] {
+        return this.outBuffer;
     }
 
     terminate() {
